7d1bd4e18c086c5a03407ca852282647b18d3d32
LeoMarqz <leomarqz2020@gmail.com>
2022-11-22 14:55:33 -0600

    adding more flags and console log handling

---
+++ libs/logger.py
@@ -0,0 +1,25 @@
+from sys import stderr
+
+class Logger:
+    def __init__(self, verbosity: bool = False):
+        # foreground code
+        self.verbosity = verbosity
+        self.__green = '\033[0;32m'
+        self.__red = '\033[0;31m'
+        self.__yellow = '\033[0;33'
+        self.__white = '\033[1;37'
+        self.__end_color = '\033[0m'
+    
+    def set_verbosity(self, verbosity: bool):
+        self.verbosity = verbosity
+    
+    def log(self, message: str):
+        if self.verbosity:
+            print(f'> {self.__green}{message}{self.__end_color}')
+    
+    def warn(self, message, file=stderr):
+        print(f'{self.__yellow}WARNING: {message}{self.__end_color}', file=file)
+    
+    def error(self, message, file= stderr):
+        print(f'{self.__red}ERROR: {message}{self.__end_color}', file=file)
+
--- main.py
+++ main.py
@@ -2,32 +2,55 @@
 import argparse
 from pathlib import Path
 from sys import stderr
-from libs.CpError import CpError
+from libs.cp_error import CpError
+from libs.logger import Logger
+
+logger = Logger()
 
 def dump(src: Path, dest: Path):
     with open(src, 'rb') as s, open(dest, 'wb') as d:
         d.write(s.read())
 
-def copy_directory(src: str, dest: str, override: bool = False):
-    print('cp dir')
+def copy_directory(src_dir: Path, dest_dir: Path, override: bool = False):
+    for src_child in src_dir.iterdir():
+        dest_child = dest_dir / src_child.name
+        if src_child.is_dir():
+            logger.log(f'Copy [dir] {src_child} -> {dest_child}')
+            dest_child.mkdir(exist_ok=True)
+            copy_directory(src_child, dest_child, override)
+        elif src_child.is_file():
+            if dest_child.is_file() and not override:
+                logger.warn(f'Skipping {src_child} -> {dest_child} as -o is not present')
+            else:
+                logger.log(f'Copy [file] {src_child} -> {dest_child}')
+                dest_child.touch()
+                dump(src_child, dest_child)
+        else:
+            logger.error(f'Skipping {src_child} because file type is not supported')
+
 
 def copy_file(src: str, dest: str, override: bool = False):
     if dest.is_dir():
         dest = dest / src.name
     if dest.is_file() and not override:
         raise CpError(f'Cannot override {dest}, specify -o option')
-    print(f'Copy: {src} -> {dest}')
+    logger.log(f'Copy: {src} -> {dest}')
     dest.touch()
     dump(src, dest)
     
 
-def copy(src: Path, dest: Path, override: bool= False):
+def copy(src: Path, dest: Path, override: bool= False, recursive: bool = False):
     if src.is_file():
         copy_file(src, dest, override)
     elif src.is_dir():
         dest_is_dir = dest.is_dir()
         if not dest_is_dir and dest.exists():
             raise CpError(f'Destination {dest} is not a directory')
+        if not recursive:
+            raise CpError(f'Skipping directory {src} because -r not present')
+        if dest_is_dir:
+            dest = dest / src.name
+        dest.mkdir(exist_ok=True)
         copy_directory(src, dest, override)
     else:
         raise CpError('file type not supported')
@@ -36,6 +59,8 @@ def cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog= 'cp', description= 'cp command implementation in python')
     parser.add_argument('-o', '--override', action='store_true', help='Override destination files if they already exist')
     parser.add_argument('-r', '--recursive', action='store_true', help='Copy directories recursively')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Give details about actions being performed')
+    parser.add_argument('-i', '--interactive', action='store_true', help='Give details about actions being performed')
     parser.add_argument('source', help= 'source directory or file', type= Path)
     parser.add_argument('destination', help= 'destination directory or file', type= Path)
     return parser.parse_args()
@@ -43,9 +68,10 @@ def cli() -> argparse.Namespace:
 def main():
     try:
         args = cli()
-        copy(args.source, args.destination, args.override)
+        logger.set_verbosity(args.verbose)
+        copy(args.source, args.destination, args.override, args.recursive)
     except CpError as ex:
-        print(ex, file=stderr) #imprime el error
+        logger.error(ex)
         exit(1)
 
 
